0bb1067d3f9a42ba55cd2aca43188d299f8e4d32
alexezm1 <alex_speed_96@hotmail.com>
2024-04-30 18:54:25 -0700

    models route in db

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 
 // Passing a connection URI
-const db = new Sequelize(process.env.POSTGRES_URL)
+const db = new Sequelize(process.env.POSTGRES_URL, {
+    models: [__dirname + '/../models/**/*.ts']
+})
 
 export default db
