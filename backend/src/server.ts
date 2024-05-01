567132f7d7fae60f33cdda0bcbb35388e676721a
alexezm1 <alex_speed_96@hotmail.com>
2024-04-30 18:22:08 -0700

    add connection to DB and use product routes

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+import db from './config/db';
+import { productRouter } from './routes/router';
+
+// Connection to DB
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+connectDB()
+const server = express()
+
+server.use('/api/products', productRouter)
+
+export default server;
