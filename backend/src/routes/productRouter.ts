5492453f2ff330fc08db6fca931098a8c415525a
alexezm1 <alex_speed_96@hotmail.com>
2024-05-01 20:29:34 -0700

    added middleware to post request

--- backend/src/routes/productRouter.ts
+++ backend/src/routes/productRouter.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+import { createProduct } from '../controllers/product'
+import { productValidation } from '../middleware'
 
 const productRouter = Router()
 
 productRouter.get('/', (req, res) => {
     res.json('Hello World')
 })
+
+productRouter.post('/', productValidation, createProduct)
 
 export { productRouter }
 
