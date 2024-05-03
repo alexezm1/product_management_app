0817e9ccb6b2bba752b2e84d8e80a71e68e4dbac
alexezm1 <alex_speed_96@hotmail.com>
2024-05-02 20:00:11 -0700

    updateProduct and updateAvailability route, middleware and controller

--- backend/src/controllers/product.ts
+++ backend/src/controllers/product.ts
@@ -33,3 +33,43 @@ export const createProduct = async (req: Request, res: Response) => {
         console.log(error)
     }
 }
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        // FIND PRODUCT
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' })
+        }
+
+        // UPDATE PRODUCT
+        await product.update(req.body)
+
+
+        res.json({ data: product })
+    } catch (error) {
+
+    }
+}
+
+export const updateAvailability = async (req: Request, res: Response) => {
+    try {
+        // FIND PRODUCT
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' })
+        }
+
+        // UPDATE PRODUCT AVAILABILITY
+        product.availability = !product.dataValues.availability
+        await product.save()
+
+        res.json({ data: product })
+    } catch (error) {
+
+    }
+}
--- backend/src/routes/productRouter.ts
+++ backend/src/routes/productRouter.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
-import { createProduct, getProductById, getProducts } from '../controllers/product'
-import { createProductValidation, getProductByIdValidation } from '../middleware'
+import { createProduct, getProductById, getProducts, updateAvailability, updateProduct } from '../controllers/product'
+import { createProductValidation, getProductByIdValidation, updateAvailabilityValidation, updateProductValidation } from '../middleware'
 
 const productRouter = Router()
 
 productRouter.get('/', getProducts)
 
 productRouter.get('/:id', getProductByIdValidation, getProductById)
+
+productRouter.put('/:id', getProductByIdValidation, updateProductValidation, updateProduct)
+
+productRouter.patch('/:id', getProductByIdValidation, updateAvailabilityValidation, updateAvailability)
 
 productRouter.post('/', createProductValidation, createProduct)
 
