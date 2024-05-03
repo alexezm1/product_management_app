1993d061827a5ae8bdb3637e9e1b0a503f773be9
alexezm1 <alex_speed_96@hotmail.com>
2024-05-03 14:41:14 -0700

    removed float decimals to remove sequelize warning

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,8 +1,10 @@
+import 'dotenv/config';
 import { Sequelize } from "sequelize-typescript";
 
 // Passing a connection URI
 const db = new Sequelize(process.env.POSTGRES_URL, {
-    models: [__dirname + '/../models/**/*.ts']
+    models: [__dirname + '/../models/**/*.ts'],
+    logging: false
 })
 
 export default db
