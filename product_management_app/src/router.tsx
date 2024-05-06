534f1b37155bddba0c8b6464ce0d98875a0c6360
alexezm1 <alex_speed_96@hotmail.com>
2024-05-05 18:12:19 -0700

    added router

---
+++ product_management_app/src/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import NewProduct from "./pages/NewProduct";
+import Products from "./pages/Products";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Products />,
+      },
+      {
+        path: "products/new",
+        element: <NewProduct />,
+      },
+    ],
+  },
+]);
