6135093f06ddce6da042c62f4764b08059092a97
alexezm1 <alex_speed_96@hotmail.com>
2024-05-06 19:25:50 -0700

    add, get, update products and get products by id

--- product_management_app/src/services/productService.ts
+++ product_management_app/src/services/productService.ts
@@ -1,12 +1,15 @@
 import axios from "axios"
-import { DraftProductSchema } from "../schemas"
+import { DraftProductSchema, ProductSchema, ProductsSchema } from "../schemas"
+import { Product } from "../types"
 
 type ProductData = {
     [k: string]: FormDataEntryValue
 }
 
+const API_URL: string = import.meta.env.VITE_API_URL
+
 export async function addProduct(data: ProductData) {
-    const API_URL: string = import.meta.env.VITE_API_URL
+
     try {
         const result = DraftProductSchema.safeParse({
             name: data.name,
@@ -17,6 +20,53 @@ export async function addProduct(data: ProductData) {
             await axios.post(url, result.data)
         } else {
             throw new Error('Invalid Data')
+        }
+    } catch (error) {
+        return error
+    }
+}
+
+export async function getProducts() {
+    try {
+        const url = `${API_URL}/api/products`
+        const response = await axios(url)
+        const result = ProductsSchema.safeParse(response.data.data)
+        if (result.success) {
+            return result.data
+        } else {
+            throw new Error('Could not fetch data')
+        }
+    } catch (error) {
+        return error
+    }
+}
+
+export async function getProductById(id: Product['id']) {
+    try {
+        const url = `${API_URL}/api/products/${id}`
+        const response = await axios(url)
+        const result = ProductSchema.safeParse(response.data.data)
+        if (result.success) {
+            return result.data
+        } else {
+            throw new Error('Could not fetch data')
+        }
+    } catch (error) {
+        return error
+    }
+}
+
+export async function updateProduct(data: ProductData, id: Product['id']) {
+    const updatedProduct = { id, ...data, price: +data.price }
+    try {
+        const url = `${API_URL}/api/products/${id}`
+        const response = await axios.put(url, updatedProduct)
+        const result = ProductSchema.safeParse(response.data.data)
+        if (result.success) {
+            console.log(result.data)
+            return result.data
+        } else {
+            throw new Error('Could not update Product')
         }
     } catch (error) {
         return error
