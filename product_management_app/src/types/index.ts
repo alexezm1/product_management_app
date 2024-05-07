e0ccae86d642122f0db225b4882d254ad20e0f10
alexezm1 <alex_speed_96@hotmail.com>
2024-05-06 19:26:09 -0700

    added types for new/updated schemas

--- product_management_app/src/types/index.ts
+++ product_management_app/src/types/index.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
-import { DraftProductSchema, ProductSchema } from "../schemas";
+import { DraftProductSchema, ProductSchema, ProductsSchema } from "../schemas";
 
 export type Product = z.infer<typeof ProductSchema>
+export type Products = z.infer<typeof ProductsSchema>
 export type DraftProduct = z.infer<typeof DraftProductSchema>
