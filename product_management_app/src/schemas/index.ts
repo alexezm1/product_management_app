60cb340097502741ade051b110f4e191e5470b59
alexezm1 <alex_speed_96@hotmail.com>
2024-05-06 19:25:17 -0700

    updated Product Schema and created Products Schema

--- product_management_app/src/schemas/index.ts
+++ product_management_app/src/schemas/index.ts
@@ -1,11 +1,14 @@
 import z from 'zod'
 
 export const ProductSchema = z.object({
+    id: z.number(),
     name: z.string(),
     price: z.number(),
     availability: z.boolean(),
     hidden: z.boolean()
 })
+
+export const ProductsSchema = z.array(ProductSchema)
 
 export const DraftProductSchema = z.object({
     name: z.string(),
