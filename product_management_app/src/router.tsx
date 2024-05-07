b407a4dae46dfaae506fa3d386b1b0c9fe935c12
alexezm1 <alex_speed_96@hotmail.com>
2024-05-06 19:26:32 -0700

    added edit product route with loader and actions

--- product_management_app/src/router.tsx
+++ product_management_app/src/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import { newProductAction } from "./actions";
+import { editProductAction, newProductAction } from "./helpers/actions";
+import { editProductLoader, productLoader } from "./helpers/loaders";
 import MainLayout from "./layouts/MainLayout";
+import EditProduct from "./pages/EditProduct";
 import NewProduct from "./pages/NewProduct";
 import Products from "./pages/Products";
 
@@ -12,11 +14,18 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <Products />,
+        loader: productLoader,
       },
       {
         path: "products/new",
         element: <NewProduct />,
         action: newProductAction,
+      },
+      {
+        path: "products/:id/edit",
+        element: <EditProduct />,
+        action: editProductAction,
+        loader: editProductLoader,
       },
     ],
   },
